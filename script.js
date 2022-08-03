bbe70fc5b44276a505f30254ae6dc2292be103c1
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2022-08-03 15:58:30 +0100

    Almost Done, I need to add video's for my algo visualiser and deploy my naruto-notions blog

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light-mode')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light-mode'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'dark-mode'){
+		document.getElementById('theme-style').href = 'dark.css'
+	}
+
+	
+
+	localStorage.setItem('theme', mode)
+}
+
+
+var myVar;
+        function myFunction() {
+            myVar = setTimeout(showPage, 2000);
+        }
+        function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        }
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
